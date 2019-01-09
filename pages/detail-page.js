6fd16769bd3f46b313d2a8afc2f077f4f503c56b
Izabela <izabela.nowosada@gmail.com>
2019-01-09 10:35:23 +0100

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,24 @@
 function submitComment(){
-    console.log("Hello");
+    // gather data
+    const inputField = document.getElementById('name');
+    let name = inputField.value;
+    const textAreaField = document.getElementById('msg');
+    let msg = textAreaField.value;
+    
+    console.log(name);
+    console.log(msg);
+    
+    // create place for new darta
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+
+    //assign the data
+    h3.innerHTML = `${name} said:`;
+    p.innerHTML = `${msg}`;
+    comment.classList.add('comment');
+    comment.appendChild(h3);
+    comment.appendChild(p);
+
+    console.log(comment);
 }
